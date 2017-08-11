4a3779355c7d0bfadaabbb7ebc3c4c7f7dea0c47
Roger Ramirez <roger8849@gmail.com>
2017-08-11 10:07:08 -0500

    changes into message sender

--- springboot-activemq/src/main/java/com/wfs/poc/messaging/MessageSender.java
+++ springboot-activemq/src/main/java/com/wfs/poc/messaging/MessageSender.java
@@ -1,26 +1,32 @@
 package com.wfs.poc.messaging;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageSender {
 
-	@Autowired
-	JmsTemplate jmsTemplate;
+  @Autowired
+  JmsTemplate jmsTemplate;
 
-	public void sendMessage(final com.wfs.poc.model.Message message) {
+  public void sendMessage(final com.wfs.poc.model.Message message) {
 
-//		jmsTemplate.send(new MessageCreator(){
-//				@Override
-//				public Message createMessage(Session session) throws JMSException{
-//					ObjectMessage objectMessage = session.createObjectMessage(message);
-//					return objectMessage;
-//				}
-//			});
-		
-		jmsTemplate.convertAndSend(message);
-	}
+    jmsTemplate.send(new MessageCreator() {
+      
+      @Override
+      public Message createMessage(Session session) throws JMSException {
+        ObjectMessage objectMessage = session.createObjectMessage(message);
+        return objectMessage;
+      }
+    });
+
+    jmsTemplate.convertAndSend(message);
+  }
 
 }
